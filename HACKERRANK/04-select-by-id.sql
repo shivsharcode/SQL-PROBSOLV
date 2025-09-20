396788f43d50203fb0c88bd9181fe054bce0fd86
Shivam Sharma <138241807+shivsharcode@users.noreply.github.com>
2025-09-20 10:25:15 +0000

    added hackerrank sql problem 2-9

---
+++ HACKERRANK/04-select-by-id.sql
@@ -0,0 +1,4 @@
+
+SELECT *   -- selecting all the columns
+FROM CITY
+WHERE ID = 1661
---
+++ HACKERRANK/05-japanese-cities-attributes.sql
@@ -0,0 +1,8 @@
+/*
+ATTRIBUTE is a column/Field in a table. e.g : ID, NAME, POPULATION etc
+TUPLES/RECORDS are the rows or entries
+*/
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN' ;
